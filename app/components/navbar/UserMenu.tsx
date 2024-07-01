a2e861d20529a4dc7c5c1e9fe85bce79ab4e797d
Tan Chun Hung <andrewtch91@gmail.com>
2024-07-01 18:09:40 +0800

    Update UserMenu.tsx

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -139,7 +139,7 @@ const UserMenu: React.FC<UserMenuProps> = ({
             </div>
             <div
               onClick={toggleOpen}
-              className="p-4 md:py-2 md:px-4 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition"
+              className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition"
             >
               {isOpen ? 'X' : <AiOutlineMenu />}
               <div className="inline-block">
